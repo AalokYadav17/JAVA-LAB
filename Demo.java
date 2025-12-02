139ee23c234945f94122c09398589f0779f2154d
AalokYadav17 <24052777@kiit.ac.in>
2025-12-02 22:52:16 +0530

    I have decleard x and print it

--- Demo.java
+++ Demo.java
@@ -3,6 +3,8 @@ public class Demo
     public static void main(String args[])
     {
         System.out.println("Hello World");
+        int x =25;
+        System.out.println("the value of x is " + x);
 
     }
 }
